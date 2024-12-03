4b3f81ceea3db0e7e4fa0b31550de51158819a6a
VenkateshDevarakonda0706 <venkateshdevarakonda0706@gmail.com>
2024-12-03 18:59:15 +0530

    Update App.java
    changing in github for js jenkins pipeline

--- src/main/java/SEGroup/MavenJavaProject1/App.java
+++ src/main/java/SEGroup/MavenJavaProject1/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "22BD1A664H" );
+        System.out.println( "Venkatesh Devarakonda" );
+        System.out.println( "changing in github for js jenkins pipeline" );
     }
 }
